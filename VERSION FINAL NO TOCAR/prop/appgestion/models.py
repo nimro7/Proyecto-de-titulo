ba1dd5d33f217d7600f75261ebcd116d28522034
nimro7 <95050523+nimro7@users.noreply.github.com>
2022-06-07 19:43:12 -0400

    contacto +

--- VERSION FINAL NO TOCAR/prop/appgestion/models.py
+++ VERSION FINAL NO TOCAR/prop/appgestion/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Contacto(models.Model):
+    id_comen =  models.AutoField(primary_key= True)
+    correo = models.CharField(max_length=50)
+    asunto = models.CharField(max_length=50)
+    comentarios = models.CharField(max_length=200)
